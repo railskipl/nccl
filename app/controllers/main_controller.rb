25dafbec418c84bbbdbef1f8fd6bc4b3b798779f
Amol <rails@kunalinfotech.net>
2012-12-19 15:35:16 +0530

    rss feed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,13 +5,40 @@ require 'open-uri'
 class MainController < ApplicationController
   
   def index
-     source = "http://feeds.business-standard.com/rss/48.xml" # url or local file
-     content = "" # raw content of rss feed will be loaded here
-      open(source) do |s| content = s.read end
+    
+     # Market News
+      source_market = "http://feeds.business-standard.com/rss/48.xml" # url or local file
+      content_market = "" # raw content of rss feed will be loaded here
+      open(source_market) do |s| content_market = s.read end
 
-        @rss = RSS::Parser.parse(content, false)
-        @feed = @rss.items
-        @feeds = @feed
+        @rss_market = RSS::Parser.parse(content_market, false)
+        @markets = @rss_market.items.first(4)
+        
+      # Banking News
+        source_banking = "http://feeds.business-standard.com/rss/3_0.xml" # url or local file
+        content_banking = "" # raw content of rss feed will be loaded here
+        open(source_banking) do |s| content_banking = s.read end
+
+          @rss_banking = RSS::Parser.parse(content_banking, false)
+          @bankings = @rss_banking.items.first(4)
+          
+      
+      # Economy News
+        source_economy = "http://feeds.business-standard.com/rss/4_0.xml" # url or local file
+        content_economy = "" # raw content of rss feed will be loaded here
+        open(source_economy) do |s| content_economy = s.read end
+
+          @rss_economy = RSS::Parser.parse(content_economy, false)
+          @economies = @rss_economy.items.first(4)
+          
+      # Companies News
+        source_company = "http://feeds.business-standard.com/rss/2_0.xml" # url or local file
+        content_company = "" # raw content of rss feed will be loaded here
+        open(source_company) do |s| content_company = s.read end
+
+           @rss_company = RSS::Parser.parse(source_company, false)
+           @companies = @rss_company.items.first(4)
+
         
   end
   
