ed94c5582489f8b418d1e0910a3f57bdc884f206
Amol <rails@kunalinfotech.net>
2012-12-20 09:59:16 +0530

    committee page update

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,7 +1,7 @@
 class Committee < ActiveRecord::Base
   attr_accessible :description, :designation, :education, :name, :photo, :committee_type_id, :address, :contact_info
   
-  has_friendly_id :name, :use_slug => true
+  
   
   has_attached_file :photo, 
                     :styles => { :thumb => "100x100" },
